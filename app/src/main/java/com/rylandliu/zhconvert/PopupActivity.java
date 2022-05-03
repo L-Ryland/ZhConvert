23667e7a8e466378f56a3b2560eddebf3a432989
lianlian <kuliantnt@gmail.com>
2022-05-04 00:18:35 +0800

    修复无法启动的bug，并整改

--- app/src/main/java/com/rylandliu/zhconvert/PopupActivity.java
+++ app/src/main/java/com/rylandliu/zhconvert/PopupActivity.java
@@ -1,99 +1,100 @@
 package com.rylandliu.zhconvert;
 
+import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+
 import com.zqc.opencc.android.lib.ChineseConverter;
 import com.zqc.opencc.android.lib.ConversionType;
 
 import java.util.stream.Stream;
 
-import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
-
 public class PopupActivity extends Activity {
 
-  private static final String TAG = "PopupActivity";
-  private ConversionType s2tConfig;
-  private ConversionType t2sConfig;
+    private static final String TAG = "PopupActivity";
+    private ConversionType s2tConfig;
+    private ConversionType t2sConfig;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-//    setContentView(R.layout.activity_popup);
-    try {
-      Intent intent = getIntent();
-      CharSequence selectText = null;
-      // get system Text
-      selectText = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
-      SharedPreferences sharedPreferences = getDefaultSharedPreferences(getApplicationContext());
-      s2tConfig = convertToConvertType(sharedPreferences.getString("S2T", "s2t.json"));
-      t2sConfig = convertToConvertType(sharedPreferences.getString("T2S", "t2s.json"));
-      if (selectText != null && s2tConfig != null && t2sConfig != null) {
-        boolean isTextAbleToConvert = compareInDepth(selectText);
-        String converted = isTextAbleToConvert ? ChineseConverter.convert(selectText.toString(),
-            t2sConfig, getApplicationContext()) : ChineseConverter.convert(selectText.toString(), s2tConfig, getApplicationContext());
-        Log.d(TAG, "onCreate: " + converted);
-        intent.putExtra(Intent.EXTRA_PROCESS_TEXT, converted);
-        setResult(RESULT_OK, intent);
-      } else {
-        Log.d(TAG, "onCreate: parse error");
-      }
-      String actionPaste = Intent.ACTION_PASTE;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Intent intent = getIntent();
+            CharSequence selectText = null;
+            // get system Text
+            selectText = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
+            SharedPreferences sharedPreferences = getDefaultSharedPreferences(getApplicationContext());
+            s2tConfig = convertToConvertType(sharedPreferences.getString("S2T", "s2t.json"));
+            t2sConfig = convertToConvertType(sharedPreferences.getString("T2S", "t2s.json"));
+            if (selectText != null && s2tConfig != null && t2sConfig != null) {
+                boolean isTextAbleToConvert = compareInDepth(selectText);
+                String converted = isTextAbleToConvert ? ChineseConverter.convert(selectText.toString(),
+                        t2sConfig, getApplicationContext()) : ChineseConverter.convert(selectText.toString(), s2tConfig, getApplicationContext());
+                Log.d(TAG, "onCreate: " + "converted= " + converted + "s2tConfig= " + s2tConfig.getValue() + " t2sConfig= " + t2sConfig.getValue());
+                intent.putExtra(Intent.EXTRA_PROCESS_TEXT, converted);
+                setResult(RESULT_OK, intent);
+            } else {
+                Log.d(TAG, "onCreate: parse error");
+            }
+            String actionPaste = Intent.ACTION_PASTE;
 
-    } finally {
-      finish();
+        } finally {
+            finish();
+        }
     }
-  }
 
-  /**
-   * conversion to convert Type
-   *
-   * @param position string name
-   * @return ConversionType
-   */
-  private ConversionType convertToConvertType(String position) {
-    switch (position) {
-      case "tw2sp.json":
-        return ConversionType.TW2SP;
-      case "s2hk.json":
-        return ConversionType.S2HK;
-      case "s2t.json":
-        return ConversionType.S2T;
-      case "s2tw.json":
-        return ConversionType.S2TW;
-      case "s2twp.json":
-        return ConversionType.S2TWP;
-      case "t2hk.json":
-        return ConversionType.T2HK;
-      case "t2s.json":
-        return ConversionType.T2S;
-      case "t2tw.json":
-        return ConversionType.T2TW;
-      case "tw2s.json":
-        return ConversionType.TW2S;
-      case "hk2s.json":
-        return ConversionType.HK2S;
+    /**
+     * conversion to convert Type
+     *
+     * @param position string name
+     * @return ConversionType
+     */
+    private ConversionType convertToConvertType(String position) {
+        switch (position) {
+            case "tw2sp.json":
+                return ConversionType.TW2SP;
+            case "s2hk.json":
+                return ConversionType.S2HK;
+            case "s2t.json":
+                return ConversionType.S2T;
+            case "s2tw.json":
+                return ConversionType.S2TW;
+            case "s2twp.json":
+                return ConversionType.S2TWP;
+            case "t2hk.json":
+                return ConversionType.T2HK;
+            case "t2s.json":
+                return ConversionType.T2S;
+            case "t2tw.json":
+                return ConversionType.T2TW;
+            case "tw2s.json":
+                return ConversionType.TW2S;
+            case "hk2s.json":
+                return ConversionType.HK2S;
+        }
+        return null;
     }
-    return null;
-  }
 
-  /**
-   * Test if the given CharSequence tested string is able to convert into another one
-   * @param original CharSequence tested string
-   * @return true if the string is able to convert, or return false.
-   */
-  private boolean compareInDepth(CharSequence original) {
-    Stream<String> toChars = original.chars().mapToObj(c->String.valueOf((char) c));
-    try {
-      toChars.forEach(c -> {
-        String converted = ChineseConverter.convert(c, t2sConfig, getApplicationContext());
-        if (!converted.equals(c)) throw new RuntimeException();
-      });
-    } catch (RuntimeException e) {
-      return true;
+    /**
+     * Test if the given CharSequence tested string is able to convert into another one
+     *
+     * @param original CharSequence tested string
+     * @return true if the string is able to convert, or return false.
+     */
+    private boolean compareInDepth(CharSequence original) {
+        Stream<String> toChars = original.chars().mapToObj(c -> String.valueOf((char) c));
+        try {
+            toChars.forEach(c -> {
+                String converted = ChineseConverter.convert(c, t2sConfig, getApplicationContext());
+                if (!converted.equals(c)) throw new RuntimeException();
+            });
+        } catch (RuntimeException e) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 }
--- app/src/main/java/com/rylandliu/zhconvert/SettingsActivity.java
+++ app/src/main/java/com/rylandliu/zhconvert/SettingsActivity.java
@@ -1,132 +1,146 @@
 package com.rylandliu.zhconvert;
 
+import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.preference.*;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
 import com.zqc.opencc.android.lib.ConversionType;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
-import java.util.Objects;
 
-import static androidx.preference.PreferenceManager.getDefaultSharedPreferences;
-
+/**
+ * Settings Activity
+ */
 public class SettingsActivity extends AppCompatActivity {
 
 
-  private static final String TAG = "SettingsActivity";
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.settings_activity);
-    FragmentManager manager = getSupportFragmentManager();
-    SettingsFragment settings = new SettingsFragment();
-    if (savedInstanceState == null) {
-
-      manager.beginTransaction()
-          .replace(R.id.convert_settings, settings, "universal")
-          .commit();
-    }
-//    ActionBar actionBar = getSupportActionBar();
-//    if (actionBar != null) {
-//      actionBar.setDisplayHomeAsUpEnabled(true);
-//    }
-    SharedPreferences sharedPreferences = getDefaultSharedPreferences(getApplicationContext());
-
-
-  }
-
-  @Override
-  public void onConfigurationChanged(@NotNull Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    SharedPreferences sharedPreferences = getDefaultSharedPreferences(getApplicationContext());
-    String lang = sharedPreferences.getString("lang", "auto");
-    Log.d(TAG, "onConfigurationChanged: " + lang);
-    getApplication().createConfigurationContext(newConfig);
-  }
-
-  public static class SettingsFragment extends PreferenceFragmentCompat {
+    private static final String TAG = "SettingsActivity";
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-      Context context = getPreferenceManager().getContext();
-      PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(context);
-      // Preference Catogory
-      PreferenceCategory convert = new PreferenceCategory(context);
-      convert.setTitle(R.string.convert_header);
-      PreferenceCategory langHeader = new PreferenceCategory(context);
-      langHeader.setTitle(R.string.universal_key);
-      // convert lists
-      // List Preference
-      ListPreference t2sPreference = new ListPreference(context);
-      ListPreference s2tPreference = new ListPreference(context);
-      ListPreference langSwitch = new ListPreference(context);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        FragmentManager manager = getSupportFragmentManager();
+        SettingsFragment settings = new SettingsFragment();
+        if (savedInstanceState == null) {
 
-      // add preference list
-      t2sPreference.setKey("T2S");
-      t2sPreference.setTitle(R.string.tr_to_sp);
-      t2sPreference.setEntryValues(new String[]{ConversionType.TW2S.getValue(), ConversionType.HK2S.getValue(), ConversionType.TW2SP.getValue(), ConversionType.T2S.getValue()});
-      t2sPreference.setEntries(R.array.t2s_options);
-      t2sPreference.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
-//      t2sPreference.setDefaultValue(R.string.t2s);
-      t2sPreference.setDefaultValue("t2s.json");
-      s2tPreference.setKey("S2T");
-      s2tPreference.setTitle(R.string.sp_to_tr);
-      s2tPreference.setEntryValues(new String[]{ConversionType.TW2T.getValue(), ConversionType.S2TWP.getValue(), ConversionType.HK2T.getValue(), ConversionType.S2TW.getValue(), ConversionType.S2T.getValue(), ConversionType.T2TW.getValue(), ConversionType.S2HK.getValue()});
-      s2tPreference.setEntries(R.array.s2t_options);
-      s2tPreference.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
-      s2tPreference.setDefaultValue("s2t.json");
-      langSwitch.setKey("lang");
-      langSwitch.setTitle(R.string.locale);
-      String auto = getResources().getString(R.string.auto);
-      langSwitch.setEntries(new String[]{"🇬🇧 English", "🇨🇳 简体中文", "🇹🇼 繁體中文", auto});
-      langSwitch.setEntryValues(new String[]{"en", "zh-cn", "zh-tw", "auto"});
-      langSwitch.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
-      langSwitch.setDefaultValue("auto");
-      langSwitch.setOnPreferenceChangeListener(this.switchLangListener);
+            manager.beginTransaction()
+                    .replace(R.id.convert_settings, settings, "universal")
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(@NotNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        SharedPreferences sharedPreferences = getDefaultSharedPreferences(getApplicationContext());
+        String lang = sharedPreferences.getString("lang", "auto");
+        Log.d(TAG, "onConfigurationChanged: " + lang);
+        getApplication().createConfigurationContext(newConfig);
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        private final Preference.OnPreferenceChangeListener switchLangListener = (preference, newValue) -> {
+            if (preference.getKey().equals("lang")) {
+                Context context = getContext();
+                Configuration configuration = getContext().getResources().getConfiguration();
+                String lang = newValue.toString();
+                switch (lang) {
+                    case "en":
+                        configuration.setLocale(Locale.ENGLISH);
+                        break;
+                    case "zh-cn":
+                        configuration.setLocale(Locale.CHINA);
+                        break;
+                    case "zh-tw":
+                        configuration.setLocale(Locale.TAIWAN);
+                        break;
+                    default:
+                        break;
+                }
+                context.createConfigurationContext(configuration);
+                return true;
+            }
+            return false;
+        };
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            Context context = getPreferenceManager().getContext();
+            PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(context);
+            // Preference Category
+            PreferenceCategory convert = new PreferenceCategory(context);
+            convert.setTitle(R.string.convert_header);
+            PreferenceCategory langHeader = new PreferenceCategory(context);
+            langHeader.setTitle(R.string.universal_key);
+            // convert lists
+            // List Preference
+            ListPreference t2sPreference = new ListPreference(context);
+            ListPreference s2tPreference = new ListPreference(context);
+            ListPreference langSwitch = new ListPreference(context);
+
+            //Conversion List
+            String[] ConversionList = new String[]{
+                    ConversionType.TW2SP.getValue(),
+                    ConversionType.S2HK.getValue(),
+                    ConversionType.S2T.getValue(),
+                    ConversionType.S2TW.getValue(),
+                    ConversionType.S2TWP.getValue(),
+                    ConversionType.T2HK.getValue(),
+                    ConversionType.T2S.getValue(),
+                    ConversionType.T2TW.getValue(),
+                    ConversionType.TW2S.getValue(),
+                    ConversionType.HK2S.getValue()
+            };
+
+            // add preference list
+            t2sPreference.setKey("T2S");
+            t2sPreference.setTitle(R.string.tr_to_sp);
+            t2sPreference.setEntryValues(ConversionList);
+            t2sPreference.setEntries(R.array.conver_options);
+            t2sPreference.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
+            t2sPreference.setDefaultValue("t2s.json");
+            s2tPreference.setKey("S2T");
+            s2tPreference.setTitle(R.string.sp_to_tr);
+            s2tPreference.setEntryValues(ConversionList);
+            s2tPreference.setEntries(R.array.conver_options);
+            s2tPreference.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
+            s2tPreference.setDefaultValue("s2t.json");
+            langSwitch.setKey("lang");
+            langSwitch.setTitle(R.string.locale);
+            String auto = getResources().getString(R.string.auto);
+            langSwitch.setEntries(new String[]{"🇬🇧 English", "🇨🇳 简体中文", "🇹🇼 繁體中文", auto});
+            langSwitch.setEntryValues(new String[]{"en", "zh-cn", "zh-tw", "auto"});
+            langSwitch.setSummaryProvider(ListPreference.SimpleSummaryProvider.getInstance());
+            langSwitch.setDefaultValue("auto");
+            langSwitch.setOnPreferenceChangeListener(this.switchLangListener);
 
 
-      // add to view
-      screen.addPreference(convert);
-      screen.addPreference(t2sPreference);
-      screen.addPreference(s2tPreference);
-      screen.addPreference(langHeader);
-      screen.addPreference(langSwitch);
+            // add to view
+            screen.addPreference(convert);
+            screen.addPreference(t2sPreference);
+            screen.addPreference(s2tPreference);
+            screen.addPreference(langHeader);
+            screen.addPreference(langSwitch);
 
-      setPreferenceScreen(screen);
+            setPreferenceScreen(screen);
+
+        }
 
     }
-    private Preference.OnPreferenceChangeListener switchLangListener = (preference, newValue) -> {
-      if (preference.getKey().equals("lang")) {
-        Context context = getContext();
-        Configuration configuration = getContext().getResources().getConfiguration();
-        String lang = newValue.toString();
-        switch (lang) {
-          case "en":
-            configuration.setLocale(Locale.ENGLISH);
-            break;
-          case "zh-cn":
-            configuration.setLocale(Locale.CHINA);
-            break;
-          case "zh-tw":
-            configuration.setLocale(Locale.TAIWAN);
-            break;
-          default:
-            break;
-        }
-        context.createConfigurationContext(configuration);
-        return true;
-      }
-      return false;
-    };
-
-  }
 }
